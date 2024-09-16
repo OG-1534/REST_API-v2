af723dc2c4b07168d08a36f9383fbcf8925f5c9a
OG-1534 <gershomochanda@gmail.com>
2024-09-16 22:51:42 +0300

    modifying connectToDatabase async function

--- app.js
+++ app.js
@@ -18,9 +18,22 @@ app.get('/', (req, res) => {
 
 
 //Connect to DB
-mongoose.connect(process.env.DB_CONNECTION, () =>
-    console.log('connected to DB')
-);
+async function connectToDatabase() {
+    try {
+        await mongoose.connect(process.env.DB_CONNECTION, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('Connected to DB');
+    } catch (error) {
+        console.error('Error connecting to DB:', error);
+        process.exit(1);
+    }
+}
+
+connectToDatabase();
 
 // Listening to SERVER
-app.listen(3000);
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
