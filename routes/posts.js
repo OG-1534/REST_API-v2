f42db7b6fe78d264e49851e8910ff7e638427a2f
OG-1534 <gershomochanda@gmail.com>
2024-09-16 23:19:57 +0300

    modifying posts routers

--- routes/posts.js
+++ routes/posts.js
@@ -2,8 +2,13 @@ const express = require('express');
 const router = express.Router();
 const post = require('../models/post');
 
-router.get('/', (req, res) => {
-    res.send('POSTS')
+router.get('/', async (req, res) => {
+    try {
+        const posts = await post.find();
+        res.json(posts);
+    } catch (err) {
+        res.json({ message: err });
+    }
 });
 
 router.post('/', async (req,res) => {
@@ -11,14 +16,12 @@ router.post('/', async (req,res) => {
         title: req.body.title,
         description: req.body.description
     });
-
-try {
-    const savedPost = await Post.save();
-    res.json(savedPost);
-} catch (err) {
-    res.json({ message: err });
+    try {
+        const savedPost = await Post.save();
+        res.json(savedPost);
+    } catch (err) {
+        res.json({ message: err });
     }
 });
-
 
 module.exports = router;
